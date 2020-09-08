dadbc4dca6be1b940512c2dce0022b17535fad7c
atBeatty <adam.t.beatt@gmail.com>
2020-09-08 15:36:49 -0700

    files_cleaned

--- lib/api.rb
+++ lib/api.rb
@@ -7,27 +7,14 @@ puts "New API"
     def initialize
         @json_response = {}
     end
-
+    
     url = "https://www.balldontlie.io/api/v1/players"
     uri = URI.parse(url)
     response = Net::HTTP.get_response(uri)
     response.body
-    JSON.parse(response.body)
+    puts json_body = JSON.parse(response.body)
+    binding.pry
     
-        # url = URI("https://www.balldontlie.io/api/v1/players")
-        # http.use_ssl = true
-        # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        binding.pry
-        
-        request = Net::HTTP::Get.new(url)
-        response = http.request(request) #response provided by API documentation
-
-        # @json_body = JSON.parse(response.body) #returns a large output of nba league info
-
-        # filtered_response = @json_body["response"][0]["seasons"] # returns just the seasons
-        # filtered_response.each do |season|
-        #     puts "#{season["season"]}"
-        # end
 end
 
 # API.new
