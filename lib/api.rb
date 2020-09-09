8caac8d2123efb7a2154655d3fd189512e0e9cfe
atBeatty <adam.t.beatt@gmail.com>
2020-09-09 12:14:49 -0700

    refined_api_filtered_stats

--- lib/api.rb
+++ lib/api.rb
@@ -2,20 +2,30 @@
 
 class API 
     # puts "Why does this show up?"
-
-    # url = "https://www.balldontlie.io/api/v1/players"
-    # uri = URI.parse(url) - ??
-    # response = Net::HTTP.get_response(uri)
-    # response.body
    
-    def self.get(data_cat)
-        url = "https://www.balldontlie.io/api/v1/#{data_cat}"
+    def self.get(data_cat=nil)
+    # players or games or teams
+        url = "https://www.balldontlie.io/api/v1/?seasons[]=2019"
         uri = URI.parse(url)
         response = Net::HTTP.get_response(uri)
         response.body
-        binding.pry
         json_response = JSON.parse(response.body) #data hash
+        binding.pry
     end
+
+    def self.get_stats
+        url = "https://www.balldontlie.io/api/v1/season_averages"
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+        json_response = JSON.parse(response.body) #data hash
+        binding.pry
+    end
+
+
+    
+
+
 
    
 
--- lib/cli.rb
+++ lib/cli.rb
@@ -8,14 +8,54 @@ class CLI
     def self.run
         puts "Welcome to Basketball Matchups"
         puts "Who is your team?"
+        # input = gets.chomp
+
         
         # onboarding...
     end
 
-    def self.list_teams
+    def self.list_players
+        teams = API.get("players")
+        teams["data"].collect do |id|
+            puts id["first_name"]
+        end
         binding.pry
-        newget = API.get("teams/1")
     end
+
+    def self.list_teams
+        teams = API.get("teams")
+        teams["data"].collect do |id|
+            puts id["abbreviation"]
+        end
+    end
+
+    
+    def self.players_on_team(team_name)
+        #?seasons[]=2018&team_ids[]=1
+        roster = []
+        page = 0
+        players = API.get("players")
+        # meta_data_pages = players["meta"]["total_pages"] # returns number of pages of info
+        while page < 25 do
+            players = API.get("players?page=#{page}")
+            
+            players["data"].each do |player|
+                if player["team"]["abbreviation"] == team_name
+                    roster << "#{player["first_name"]} #{player["last_name"]}"
+                end
+            end
+        page +=1
+        end
+    puts roster
+    end
+
+
+
+
+
+
+
+
 
 
 
